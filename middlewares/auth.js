2a3a665e615566d455a90e4237dd8db8de951a2f
Phonekham <phone.madridista@gmail.com>
2021-09-11 11:15:07 +0700

    fix firebase adminn export & checkauth

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,6 +1,17 @@
 const admin = require("../firebase");
 
-exports.authCheck = (req, res, next) => {
-  console.log(req.headers); // token
-  next();
+exports.authCheck = async (req, res, next) => {
+  try {
+    const firebaseUser = await admin
+      .auth()
+      .verifyIdToken(req.headers.authtoken);
+    req.user = firebaseUser;
+    console.log("firebase user auth check", firebaseUser);
+    next();
+  } catch (err) {
+    console.log(err);
+    res.status(401).json({
+      err: "invalid or expire token",
+    });
+  }
 };
