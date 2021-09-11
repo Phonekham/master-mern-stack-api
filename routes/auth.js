51eea80cca51eaaf6ef2904b422b191e5f3c8601
Phonekham <phone.madridista@gmail.com>
2021-09-11 11:58:23 +0700

    currentUser endpoint

--- routes/auth.js
+++ routes/auth.js
@@ -5,8 +5,9 @@ const router = express.Router();
 // Middlewares
 const { authCheck } = require("../middlewares/auth");
 // controller
-const { createOrUpdateUser } = require("../controllers/auth");
+const { createOrUpdateUser, currentUser } = require("../controllers/auth");
 
 router.post("/create-or-update-user", authCheck, createOrUpdateUser);
+router.post("/current-user", authCheck, currentUser);
 
 module.exports = router;
