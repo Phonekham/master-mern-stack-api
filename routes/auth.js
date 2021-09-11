6869c04f41c32b092a45bbf9833d1d6026d3709d
Phonekham <phone.madridista@gmail.com>
2021-09-11 10:42:53 +0700

    middleware

--- routes/auth.js
+++ routes/auth.js
@@ -2,9 +2,11 @@ const express = require("express");
 
 const router = express.Router();
 
+// Middlewares
+const { authCheck } = require("../middlewares/auth");
 // controller
 const { createOrUpdateUser } = require("../controllers/auth");
 
-router.post("/create-or-update-user", createOrUpdateUser);
+router.post("/create-or-update-user", authCheck, createOrUpdateUser);
 
 module.exports = router;
