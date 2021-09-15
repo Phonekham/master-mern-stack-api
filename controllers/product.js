f35b0450dff963a59a54a3f5d3209ad375df06eb
Phonekham <phone.madridista@gmail.com>
2021-09-15 14:17:01 +0700

    create product

---
+++ controllers/product.js
@@ -0,0 +1,14 @@
+const slugify = require("slugify");
+
+const Product = require("../models/product");
+
+exports.create = async (req, res) => {
+  try {
+    req.body.slug = slugify(req.body.title);
+    const newProduct = await new Product(req.body).save();
+    res.json(newProduct);
+  } catch (err) {
+    console.log(err);
+    res.status(400).send("Create product failed");
+  }
+};
